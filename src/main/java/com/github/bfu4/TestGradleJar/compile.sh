7e5ef70b6cbe26dfe5609d39117df38f7a497222
bfu4 <bella@sleeeepyti.me>
2021-02-08 17:34:57 -0500

    bruh

--- src/main/java/com/github/bfu4/TestGradleJar/compile.sh
+++ src/main/java/com/github/bfu4/TestGradleJar/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-JAVA="/usr/lib/jvm/java-11-openjdk-amd64/"
+JAVA="$JAVA_HOME"
 g++ -c -fPIC -I"$JAVA"/include -I"$JAVA"/include/linux src/main/java/com/github/bfu4/TestGradleJar/NativeImpl.cc -o src/main/java/com/github/bfu4/TestGradleJar/NativeImpl.o
 
 g++ -shared -fPIC -o src/main/java/com/github/bfu4/TestGradleJar/libNativeImpl.so src/main/java/com/github/bfu4/TestGradleJar/NativeImpl.o -lc
