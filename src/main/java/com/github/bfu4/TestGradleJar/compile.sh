39e9c239000763884f43e8547ff27d2a277deabf
bfu4 <bella@sleeeepyti.me>
2021-02-08 17:23:22 -0500

    ahrdcode buonk

--- src/main/java/com/github/bfu4/TestGradleJar/compile.sh
+++ src/main/java/com/github/bfu4/TestGradleJar/compile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 JAVA="/usr/lib/jvm/java-11-openjdk-amd64/"
-g++ -c -fPIC -I"$JAVA"/include -I"$JAVA"/include/linux NativeImpl.cc -o NativeImpl.o
+g++ -c -fPIC -I"$JAVA"/include -I"$JAVA"/include/linux src/main/java/bfu4/TestGradleJar/NativeImpl.cc -o src/main/java/bfu4/TestGradleJar/NativeImpl.o
 
-g++ -shared -fPIC -o libNativeImpl.so NativeImpl.o -lc
+g++ -shared -fPIC -o src/main/java/bfu4/TestGradleJar/libNativeImpl.so src/main/java/bfu4/TestGradleJar/NativeImpl.o -lc
 
-javac Main.java NativeImpl.java
+# javac Main.java NativeImpl.java
