827a8573797b9f1900daf11fd89a9d84b0b8a642
bfu4 <bella@sleeeepyti.me>
2021-02-17 21:22:04 -0500

    this was a bit important..

--- compile.sh
+++ compile.sh
@@ -6,6 +6,8 @@ RESDIR="src/main/resources"
 
 HEADER_PREF="com_github_bfu4_JNITravisExample_"
 
+mkdir src/main/resources
+
 g++ -c -fPIC -I"$JAVA_HOME"/include -I"$JAVA_HOME"/include/linux "$CPPDIR"/"$HEADER_PREF"NativeImpl.cc -o "$CPPDIR"/"$HEADER_PREF"NativeImpl.o
 
 g++ -shared -fPIC -o "$RESDIR"/libNativeImpl.so "$CPPDIR"/"$HEADER_PREF"NativeImpl.o -lc
